3edbb1a41c591db4a17845ac042039d95f8c3577
Ben McLean <benmclean007@gmail.com>
2024-06-06 20:59:59 +1000

    IDK

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 
 echo "$EXTENSION_KEY" > /key.pem
 
-cp /github/workspace/$1/* /extension
+cp /github/workspace/$1/* /extension/
 
 chromium --no-sandbox --pack-extension=/extension --pack-extension-key=/key.pem
+
+ls /
 
 rm /key.pem
 
