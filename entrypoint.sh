6645e545be8dca477a745fb3a7a94848c4a800ff
Ben McLean <benmclean007@gmail.com>
2024-06-06 21:01:49 +1000

    Make /extension folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,10 @@
 
 echo "$EXTENSION_KEY" > /key.pem
 
+mkdir /extension
 cp /github/workspace/$1/* /extension/
 
 chromium --no-sandbox --pack-extension=/extension --pack-extension-key=/key.pem
-
-ls /
 
 rm /key.pem
 
